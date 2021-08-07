6a6dcf5960e7600461df911e43a2ac9c3a461801
Faiziiii <sfaiz.me@gmail.com>
2021-08-07 10:45:21 +0500

    Extend PDFViewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {pdfDefaultOptions} from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pdf-extend';
+  constructor(){
+  }
+  docURL="/assets/handouts.pdf";
 }
